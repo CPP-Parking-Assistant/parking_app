1b9df5f11ff29300374002ca67269d069d0eb411
emricca <koolaidyogurt@gmail.com>
2015-05-26 22:32:24 -0700

    no map marker check now works
    checking if no markers exists now works

--- app/src/main/java/com/cpp/elliot/cppparkingassistant/ParkForm2Activity.java
+++ app/src/main/java/com/cpp/elliot/cppparkingassistant/ParkForm2Activity.java
@@ -21,6 +21,8 @@ import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 public class ParkForm2Activity extends Activity {
@@ -28,19 +30,13 @@ public class ParkForm2Activity extends Activity {
     private LatLng location;
     private LatLng markerLocation;
     private String description = "";
-    /*
-    private String description2 = "";
-    private String gender;
-    private String broncoid;
-    */
     private String type;
-    private boolean flag = false;
     private final LatLng CPP = new LatLng(34.0564,-117.8217);
-    Button parkForm2Button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.parkform2);
+        deleteOvertime();
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.parkMap)).getMap();
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(CPP, 15);
         map.animateCamera(update);
@@ -50,11 +46,7 @@ public class ParkForm2Activity extends Activity {
             @Override
             public void done(List<RideStudent> list, ParseException e) {
                 if (e == null) {
-                    if(list.size() == 0){
-                        flag = true;
-                    }
                     for (int i = 0; i < list.size(); i++) {
-
                         location = new LatLng(list.get(i).getLatitude(), list.get(i).getLongitude());
                         description = list.get(i).getGender() + ", " + list.get(i).getDescription();
                         map.addMarker(new MarkerOptions().position(location).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)).title(description));
@@ -68,13 +60,6 @@ public class ParkForm2Activity extends Activity {
             @Override
             public void done(List<LeavingStudent> list, ParseException e) {
                 if (e == null) {
-                    if(list.size() == 0 && flag)
-                        alert();
-                    for(int i = 0; i < list.size(); i++){
-                        if(checkDate(list.get(i).getDateAdded())){
-                            //do stuff
-                        }
-                    }
                     for (int i = 0; i < list.size(); i++) {
                         location = new LatLng(list.get(i).getLatitude(), list.get(i).getLongitude());
                         description = list.get(i).getDescription();
@@ -83,6 +68,8 @@ public class ParkForm2Activity extends Activity {
                 }
             }
         });
+        checkEmptyMap();
+        //Toast.makeText(ParkForm2Activity.this,b+"", Toast.LENGTH_SHORT).show();
         map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
@@ -95,7 +82,7 @@ public class ParkForm2Activity extends Activity {
                 return true;
             }
         });
-        parkForm2Button = (Button) findViewById(R.id.parkForm2Button);
+        Button parkForm2Button = (Button) findViewById(R.id.parkForm2Button);
         parkForm2Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -107,9 +94,71 @@ public class ParkForm2Activity extends Activity {
                     b.putDouble("longitude", markerLocation.longitude);
                     intent.putExtras(b);
                     startActivity(intent);
-                }
-                catch(Exception e){
+                } catch (Exception e) {
                     Toast.makeText(ParkForm2Activity.this, "Please select a student or a parking spot", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    public void deleteOvertime(){
+        ParseQuery<RideStudent> query1 = ParseQuery.getQuery("RideStudent");
+        query1.whereExists("Latitude");
+        query1.findInBackground(new FindCallback<RideStudent>() {
+            @Override
+            public void done(List<RideStudent> list, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < list.size(); i++) {
+                        if (!checkDate(list.get(i).getDateAdded())) {
+                            list.get(i).deleteInBackground();
+                            list.get(i).saveInBackground();
+                        } else if (!checkTime(list.get(i).getTimeAdded())) {
+                            list.get(i).deleteInBackground();
+                            list.get(i).saveInBackground();
+                        }
+                    }
+                }
+            }
+        });
+        ParseQuery<LeavingStudent> query2 = ParseQuery.getQuery("LeavingStudent");
+        query2.whereExists("Latitude");
+        query2.findInBackground(new FindCallback<LeavingStudent>() {
+            @Override
+            public void done(List<LeavingStudent> list, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < list.size(); i++) {
+                        if (!checkDate(list.get(i).getDateAdded())) {
+                            list.get(i).deleteInBackground();
+                            list.get(i).saveInBackground();
+                        } else if (!checkTime(list.get(i).getTimeAdded())) {
+                            list.get(i).deleteInBackground();
+                            list.get(i).saveInBackground();
+                        }
+                    }
+                }
+            }
+        });
+    }
+    public void checkEmptyMap(){
+        ParseQuery<RideStudent> query1 = ParseQuery.getQuery("RideStudent");
+        query1.whereExists("Latitude");
+        query1.findInBackground(new FindCallback<RideStudent>() {
+            @Override
+            public void done(List<RideStudent> list, ParseException e) {
+                if (e == null) {
+                    if(list.isEmpty()){
+                        ParseQuery<LeavingStudent> query2 = ParseQuery.getQuery("LeavingStudent");
+                        query2.whereExists("Latitude");
+                        query2.findInBackground(new FindCallback<LeavingStudent>() {
+                            @Override
+                            public void done(List<LeavingStudent> list, ParseException e) {
+                                if (e == null) {
+                                    if(list.isEmpty()){
+                                        alert();
+                                    }
+                                }
+                            }
+                        });
+                    }
                 }
             }
         });
@@ -127,19 +176,35 @@ public class ParkForm2Activity extends Activity {
         });
         builder.show();
     }
-    // 5-20-15
     public boolean checkDate(String date){
         int month = getMonth(date);
         int day = getDay(date);
         int year = getYear(date);
-        
-
-        return true;
+        boolean bool = false;
+        if(year == getCurrentYear()){
+            if(month == getCurrentMonth()){
+                if(day == getCurrentDay()){
+                    bool = true;
+                }
+            }
+        }
+        return bool;
     }
     public boolean checkTime(String time){
-
-
-        return true;
+        int hour = getHour(time);
+        int minute = getMinute(time);
+        boolean bool = false;
+        if(hour == getCurrentHour()){
+            if(getCurrentMinute()-minute <= 20){
+                bool = true;
+            }
+        }
+        else if(getCurrentHour() == hour+1){
+            if(getCurrentMinute()-minute+60 <= 20){
+                bool = true;
+            }
+        }
+        return bool;
     }
     public int getMonth(String date){
         String str = "";
@@ -169,5 +234,51 @@ public class ParkForm2Activity extends Activity {
             str += date.charAt(i);
         }
         return Integer.parseInt(str);
+    }
+    public int getHour(String date){
+        String str = "";
+        int index = date.indexOf(':');
+        for(int i = 0; i < index; i++){
+            str += date.charAt(i);
+        }
+        return Integer.parseInt(str);
+    }
+    public int getMinute(String date){
+        String str = "";
+        int index = date.indexOf(':')+1;
+        for(int i = index; i < date.length(); i++){
+            str += date.charAt(i);
+        }
+        return Integer.parseInt(str);
+    }
+    public int getCurrentMonth(){
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yy");
+        String date = sdf.format(c.getTime());
+        return getMonth(date);
+    }
+    public int getCurrentDay(){
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yy");
+        String date = sdf.format(c.getTime());
+        return getDay(date);
+    }
+    public int getCurrentYear(){
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yy");
+        String date = sdf.format(c.getTime());
+        return getYear(date);
+    }
+    public int getCurrentHour(){
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String date = sdf.format(c.getTime());
+        return getHour(date);
+    }
+    public int getCurrentMinute(){
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String date = sdf.format(c.getTime());
+        return getMinute(date);
     }
 }
