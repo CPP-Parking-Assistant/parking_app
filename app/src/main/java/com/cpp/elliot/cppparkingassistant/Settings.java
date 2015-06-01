9d33ba1f3ae26106954fae67e2062a2949a50cbc
emricca <koolaidyogurt@gmail.com>
2015-06-01 11:48:59 -0700

    added toast message upon deletion

--- app/src/main/java/com/cpp/elliot/cppparkingassistant/Settings.java
+++ app/src/main/java/com/cpp/elliot/cppparkingassistant/Settings.java
@@ -10,12 +10,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -23,6 +25,8 @@ import java.util.TimerTask;
 public class Settings extends ActionBarActivity {
     final private String broncoId = ParseInstallation.getCurrentInstallation().getString("Bronco");
     private Button deleteButton;
+    private boolean found = false;
+    ArrayList<String> id = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -46,6 +50,7 @@ public class Settings extends ActionBarActivity {
                         });
                     }
                 }, 500);
+                checkFail();
                 ParseQuery<RideStudent> query1 = ParseQuery.getQuery("RideStudent");
                 query1.whereExists("Latitude");
                 query1.findInBackground(new FindCallback<RideStudent>() {
@@ -56,6 +61,7 @@ public class Settings extends ActionBarActivity {
                                 if (list.get(i).getBroncoId().equals(broncoId)) {
                                     list.get(i).deleteInBackground();
                                     list.get(i).saveInBackground();
+                                    Toast.makeText(Settings.this, "Delete Successful", Toast.LENGTH_SHORT).show();
                                 }
                             }
                         }
@@ -67,15 +73,46 @@ public class Settings extends ActionBarActivity {
                     @Override
                     public void done(List<LeavingStudent> list, ParseException e) {
                         if (e == null) {
-                            for(int i = 0; i < list.size(); i++){
-                                if(list.get(i).getBroncoId().equals(broncoId)) {
+                            checkFail();
+                            for (int i = 0; i < list.size(); i++) {
+                                if (list.get(i).getBroncoId().equals(broncoId)) {
                                     list.get(i).deleteInBackground();
                                     list.get(i).saveInBackground();
+                                    Toast.makeText(Settings.this, "Delete Successful", Toast.LENGTH_SHORT).show();
                                 }
                             }
                         }
                     }
                 });
+            }
+        });
+    }
+    public void checkFail(){
+        ParseQuery<RideStudent> query1 = ParseQuery.getQuery("RideStudent");
+        query1.whereExists("Latitude");
+        query1.findInBackground(new FindCallback<RideStudent>() {
+            @Override
+            public void done(List<RideStudent> list, ParseException e) {
+                if (e == null) {
+                    for(int i = 0; i < list.size(); i++){
+                        id.add(list.get(i).getBroncoId());
+                    }
+                    ParseQuery<LeavingStudent> query2 = ParseQuery.getQuery("LeavingStudent");
+                    query2.whereExists("Latitude");
+                    query2.findInBackground(new FindCallback<LeavingStudent>() {
+                        @Override
+                        public void done(List<LeavingStudent> list, ParseException e) {
+                            if (e == null) {
+                                for(int i = 0; i < list.size(); i++){
+                                    id.add(list.get(i).getBroncoId());
+                                }
+                                if(!id.contains(broncoId)){
+                                    Toast.makeText(Settings.this, "Delete Unuccessful", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    });
+                }
             }
         });
     }
